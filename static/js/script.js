9378ada7726d140ad7ce6faf37eed5bf807e1673
ANSH KUSHWAHA <anshkushwaha2001@gmail.com>
2023-06-13 21:01:24 +0530

    Test

--- static/js/script.js
+++ static/js/script.js
@@ -38,7 +38,7 @@ const Home = {
       </div>
       <section class="category-grid">
         <div v-for="category in response" class="category">
-          <router-link :to="{ name: '', params: { category: category.name } }">
+          <router-link :to="{ name: '#', params: { category: category.name } }">
             <img :src="category.image" :alt="category.name"></a>
             <h3> {{ category.name }} </h3>
           </router-link>
@@ -111,7 +111,7 @@ const Category = {
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/:category', component: Category },
+  { path: '#/:category', component: Category },
 ]
 
 const router = VueRouter.createRouter({
