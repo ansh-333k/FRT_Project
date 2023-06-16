58a675e704035de03e83bb692775a1b01d968c11
Prakash Bist <prakashcena540@gmail.com>
2023-06-16 12:49:38 +0530

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -64,44 +64,26 @@ const Category = {
     .catch(error => { console.log(error) })
   },
   template: `
-  <div v-for="place in response" class="destination-card">
-  <div class="slider">
-  <div class="images">
-      <input type="radio" name="slide" id="img1" checked>
-      <input type="radio" name="slide" id="img2">
-      <input type="radio" name="slide" id="img3">
-      <input type="radio" name="slide" id="img4">
-      <input type="radio" name="slide" id="img5">
-      <input type="radio" name="slide" id="img6">
-
-      <img :src="place.img1" class="m1" alt="img1">
-      <img :src="place.img2" class="m2" alt="img2">
-      <img :src="place.img3" class="m3" alt="img3">
-      <img :src="place.img4" class="m4" alt="img4">
-      <img :src="place.img5" class="m5" alt="img5">
-      <img :src="place.img6" class="m6" alt="img6">
-
-  </div>
-  <div class="dots">
-      <label for="img1"></label>
-      <label for="img2"></label>
-      <label for="img3"></label>
-      <label for="img4"></label>
-      <label for="img5"></label>
-      <label for="img6"></label>
-  </div>
-  <div class="destination-details" style="background: linear-gradient(to top,rgb(0, 221, 255), #ffffff);">
-      <div class="destination">
-        <h2 class="destination-name">{{ place.name }}</h2>
-        <div class="rating">Rating: {{ place.rating }}/5</div>
+    <div v-for="place in response" class="destination-card">
+    <div class="imgs-grid">
+          <div class="imgs"><img :src="place.img1" alt="Image 1"></div>
+          <div class="imgs"><img :src="place.img2" alt="Image 2"></div>
+          <div class="imgs"><img :src="place.img3" alt="Image 3"></div>
+          <div class="imgs"><img :src="place.img4" alt="Image 4"></div>
+          <div class="imgs"><img :src="place.img5" alt="Image 5"></div>
+          <div class="imgs"><img :src="place.img6" alt="Image 6"></div>
+    </div>
+      <div class="destination-details" style="background: linear-gradient(to top,rgb(0, 221, 255), #ffffff);">
+        <div class="destination">
+          <h2 class="destination-name">{{ place.name }}</h2>
+          <div class="rating">Rating: {{ place.rating }}/5</div>
+        </div>
+        <p class="location">Location: {{ place.location }}</p>
+        <p class="caption">{{ place.caption }}</p>
+        <p class="best-time">Best Time to Visit: {{ place.best_time }}</p>
+        <p class="text">{{ place.description }}</p>
       </div>
-      <p class="location">Location: {{ place.location }}</p>
-      <p class="caption">{{ place.caption }}</p>
-      <p class="best-time">Best Time to Visit: {{ place.best_time }}</p>
-      <p class="text">{{ place.description }}</p>
-    </div>      
-</div>
-</div>
+    </div>
   ` 
 }
 
